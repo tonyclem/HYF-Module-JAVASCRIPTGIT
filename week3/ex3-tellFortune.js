4347344eed533265b636621aa484ed8280214489
tonyclem <tonyclem001@gmail.com>
2022-02-15 12:41:28 +0100

    make some correction, and additions

--- week3/ex3-tellFortune.js
+++ week3/ex3-tellFortune.js
@@ -1,42 +1,16 @@
 "use strict";
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Homework/tree/main/1-JavaScript/Week3#exercise-3-be-your-own-fortune-teller
-Why pay a fortune teller when you can just program your fortune yourself?
-1. Create four arrays, `numKids`, `partnerNames`, `locations` and `jobTitles`. 
-   Give each array five random values that have to do with the name of 
-   the variable.
-2. Complete the function `selectRandomly`. This function should take an array 
-   as a parameter and return a randomly selected element as its return value.
-3. Complete the function named `tellFortune` as follows:
-   - It should take four arguments (in the order listed): 
-     * the array with the options for the number of children, 
-     * the array with the options for the partner's name, 
-     * the array with the options for the geographic location and 
-     * the array with the options for the job title.
-   - It should use the `selectRandomly` function to randomly select values from 
-     the arrays.
-   - It should return a string: "You will be a `jobTitle` in `location`, 
-    married to `partnerName` with `numKids` kids."
-4. Call the function three times, passing the arrays as arguments. Use `
-   console.log` to display the results.
-Note: The DRY principle is put into practice here: instead of repeating the code to 
-randomly select array elements four times inside the `tellFortune` function 
-body, this code is now written once only in a separated function.
------------------------------------------------------------------------------*/
 
-// This function should take an array as its parameter and return
-// a randomly selected element as its return value.
 function selectRandomly(array) {
-  // TODO complete this function
-  const randomArr = Math.floor(Math.random() * array);
-  return randomArr;
+  return array[Math.floor(Math.random() * array.length)];
 }
 
 function tellFortune(numKids, partnerNames, locations, jobTitles) {
   // TODO complete this function
-
-  const randomEl = selectRandomly();
-  return `You will be a ${jobTitles[randomEl]} in ${locations[randomEl]}, married to ${partnerNames[randomEl]} with ${numKids[randomEl]} kids.`;
+  const rdNumberOfKids = selectRandomly(numKids);
+  const rdPartnerNames = selectRandomly(partnerNames);
+  const rdLocations = selectRandomly(locations);
+  const rdJobTitles = selectRandomly(jobTitles);
+  return `You will be a ${rdJobTitles} in ${rdLocations}, married to ${rdPartnerNames} with ${rdNumberOfKids} kids.`;
 }
 
 function main() {
