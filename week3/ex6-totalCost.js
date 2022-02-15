38ed029a7cc1ed964b34138017407aadce2f2912
tonyclem <tonyclem001@gmail.com>
2022-02-15 19:09:16 +0100

    making some correction

--- week3/ex6-totalCost.js
+++ week3/ex6-totalCost.js
@@ -1,50 +1,33 @@
 "use strict";
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Homework/tree/main/1-JavaScript/Week3#exercise-6-total-cost-is
-You want to buy a couple of things from the supermarket to prepare for a party.
-After scanning all the items the cashier wants to give you the total price, but
-the machine is broken! Let's write her a function that does it for her
-instead!
-1. Create an object named `cartForParty` with five properties. Each property
-   should be a grocery item (like `beers` or `chips`) and hold a number value
-   (like `1.75` or `0.99`).
-2. Complete the function called `calculateTotalPrice`.
-   - It takes one parameter: an object that contains properties that only contain
-     number values.
-   - Loop through the object and add all the number values together.
-   - Return a string: "Total: €`amount`".
-3. Complete the unit test functions and verify that all is working as expected.
------------------------------------------------------------------------------*/
+
 const cartForParty = {
   // TODO complete this object
-  water: "water",
-  drink: "beers",
-  chips: "chips",
-  num1: 1.75,
-  num2: 0.99,
+  water: "1.3",
+  drink: "1.4",
+  chips: "2.4",
+  apple: "1.75",
+  orange: "0.99",
 };
 
-function calculateTotalPrice(number) {
-  let total = cartForParty.num1 + cartForParty.num2;
-
-  //   let total = 0;
-  //   for (let cart of number) {
-  //     total += cart;
-  //   }
-  console.log(`Total: €${total}`);
+function calculateTotalPrice(myObject) {
+  let total = 0;
+  for (let [key, obj] of Object.entries(myObject)) {
+    total += parseFloat(`${obj}`);
+  }
+  return `Total: €${total}`;
 }
 
+calculateTotalPrice(cartForParty);
 // ! Test functions (plain vanilla JavaScript)
 function test1() {
   console.log("\nTest 1: calculateTotalPrice should take one parameter");
   // TODO replace this comment with your code
-  calculateTotalPrice();
 }
 
 function test2() {
   console.log("\nTest 2: return correct output when passed cartForParty");
   // TODO replace this comment with your code
-  console.log(cartForParty);
+  calculateTotalPrice(cartForParty);
 }
 
 function test() {
